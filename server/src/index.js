8dd809416c0a305b80ee458e0a79fe0dbf5d0228
lancer <jbkhanhtran@gmail.com>
2023-04-12 11:37:21 +0700

    set up base for server

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
