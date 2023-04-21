049c6b917624d95f9015a62bf9683e6c3453fb11
phongbj <20520364@gm.uit.edu.vn>
2023-04-21 08:46:41 +0700

    api Search fix

--- app/App.js
+++ app/App.js
@@ -1,21 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-<<<<<<< Updated upstream
-import LoginScreen from "./src/screens/Login.screen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <LoginScreen></LoginScreen>
-    </View>
-  );
-=======
 import SearchScreen from "./src/screens/Search.screen";
-import Navigator from "./src/navigations/main.navigator";
+
 
 export default function App() {
   return <SearchScreen></SearchScreen>;
->>>>>>> Stashed changes
+
 }
 
 const styles = StyleSheet.create(   {
