ceaaeaf07342839e676365f4fb637f8b116aebe2
lancer2672 <JBkhanhtran@gmail.com>
2023-07-06 15:58:14 +0700

    clean code

--- server/src/controllers/notification.controller.js
+++ server/src/controllers/notification.controller.js
@@ -1,12 +1,65 @@
 const db = require("../db");
-
+const sendNotification = require("../notification");
 const notificationRef = db.collection("notifications");
 
-exports.saveNotificationToDb = async (notification) => {
+const saveNotificationToDb = async (notification) => {
   try {
     await notificationRef.add(notification);
     console.log("saved - userId", notification.userId);
   } catch (err) {
     console.log("Error when saving notification:", err);
+  }
+};
+
+exports.notificationToUsers = async (req, res) => {
+  const { userData, title, message } = req.body;
+  console.log("userData", userData);
+  if (title != null && message != null) {
+    for (const data of userData) {
+      const { notificationToken, userId } = data;
+      const notification = {
+        title,
+        message,
+        date: new Date(),
+        userId: userId,
+        read: false,
+      };
+      sendNotification(notificationToken, title, message);
+      await saveNotificationToDb(notification);
+    }
+    res.json({
+      message: "success",
+    });
+  } else {
+    res.json({
+      message: "failed",
+    });
+  }
+};
+
+exports.notify = async (req, res) => {
+  const { token, userId, title, message } = req.body;
+  console.log({ token, message });
+  if (token != null) {
+    sendNotification(token, title, message);
+    const notification = {
+      title,
+      message,
+      date: new Date(),
+      userId: userId,
+      read: false,
+    };
+    await saveNotificationToDb(notification);
+    res.json({
+      message: "success",
+      data: {
+        token,
+        message,
+      },
+    });
+  } else {
+    res.json({
+      message: "failed",
+    });
   }
 };
--- server/src/index.js
+++ server/src/index.js
@@ -1,54 +1,12 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const sendNotification = require("./notification");
 const notificationController = require("./controllers/notification.controller");
+
 app.use(express.json());
 
-app.post("/notify", (req, res) => {
-  const { token, title, message } = req.body;
-  console.log({ token, message });
-  if (token != null) {
-    sendNotification(token, title, message);
-    res.json({
-      message: "success",
-      data: {
-        token,
-        message,
-      },
-    });
-  } else {
-    res.json({
-      message: "failed",
-    });
-  }
-});
-
-app.post("/notifyToUsers", (req, res) => {
-  const { userData, title, message } = req.body;
-  console.log("userData", userData);
-  if (title != null && message != null) {
-    userData.forEach((data) => {
-      const { notificationToken, userId } = data;
-      const notification = {
-        title,
-        message,
-        date: new Date(),
-        userId: userId,
-        read: false,
-      };
-      sendNotification(notificationToken, title, message);
-      notificationController.saveNotificationToDb(notification);
-    });
-    res.json({
-      message: "success",
-    });
-  } else {
-    res.json({
-      message: "failed",
-    });
-  }
-});
+app.post("/notify", notificationController.notify);
+app.post("/notifyToUsers", notificationController.notificationToUsers);
 
 const port = process.env.PORT || 3000;
 
